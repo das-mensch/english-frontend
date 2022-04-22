9973a56916f044d4b0ac344333b328eccb5dbff9
Jan Hoffmann <jan.hoffmann1988@live.de>
2022-04-22 20:50:52 +0200

    feat: add look and feel inspired by persona 5 chat app

--- src/atomic/atoms/Textarea.tsx
+++ src/atomic/atoms/Textarea.tsx
@@ -1,11 +1,19 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledTextArea = styled.textarea`
+  font-family: inherit;
+`;
+
 type TextareaProps = {
   value?: string;
+  placeholderText?: string;
   onInput?: (text: string) => void
 }
 
 export const Textarea = (props: TextareaProps) => {
-  const { value, onInput } = props;
+  const { value, onInput, placeholderText } = props;
   return (
-    <textarea value={value} onInput={(evt) => onInput !== undefined && onInput(evt.currentTarget.value) }></textarea>
-  )
-}
+    <StyledTextArea placeholder={placeholderText} value={value} onInput={(evt) => onInput !== undefined && onInput(evt.currentTarget.value) }></StyledTextArea>
+  );
+};
--- src/atomic/organisms/SentenceChecker.tsx
+++ src/atomic/organisms/SentenceChecker.tsx
@@ -1,29 +1,80 @@
-import { useEffect, useState } from 'react';
-import styled from 'styled-components';
+import React, { useEffect, useState } from 'react';
+import styled, { keyframes } from 'styled-components';
 import { checkSentence } from '../../service/checkSentence';
 import { ResponseState, ResponseWrapper } from '../../types';
 import { TextareaWithButton } from '../molecules/TextareaWithButton';
 
+const skewAnimation = keyframes`
+  from {
+    transform: skew(0deg, 0deg) scale(1);
+  }
+  to {
+    transform: skew(15deg, 0deg) scale(1.1);
+  }
+`;
+
+const pendingAnimation = keyframes`
+  0% {
+    content: "\u00b7";
+  }
+  50% {
+    content: "\u00b7\u00b7";
+  }
+  100% {
+    content: "\u00b7\u00b7\u00b7";
+  }
+`;
+
+const ChatLayout = styled.div`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  p.text {
+    margin: 0;
+    background-color: black;
+    color: white;
+    border: 0.25rem solid white;
+    padding: 1rem;
+    transform: skew(-14deg, 2deg);
+  }
+  p.pending {
+    margin: 0;
+    width: 1.5rem;
+    background-color: #660200;
+    color: #a40000;
+    padding: 0.25rem 1rem;
+    font-size: 2rem;
+    transform: rotate(-2deg) skew(-14deg, 2deg);
+  }
+  p.pending:after {
+    content: "";
+    animation: ${pendingAnimation} 500ms linear alternate infinite;
+  }
+`;
+
 const StyledTextAreaWithButton = styled.div`
   display: flex;
   flex-direction: column;
-  align-items: flex-start;
-  row-gap: 0.5rem;
+  row-gap: 1rem;
   textarea {
-    min-width: 450px;
-    min-height: 150px;
-    border: 1px solid black;
+    &:focus-visible {
+      outline: none;
+    }
+    border: 0.25rem solid white;
+    padding: 1rem;
+    background-color: black;
+    transform: skew(14deg, 2deg);
+    color: white;
     resize: none;
-    padding: 0.5rem;
   }
   button {
+    background-color: black;
+    color: white;
+    border: 0.25rem solid white;
+    animation: ${skewAnimation} 200ms ease-in-out alternate infinite;
     padding: 0.5rem;
-    background-color: #CECECE;
-    border: 1px solid black;
-    border-radius: 0.5rem;
-  }
-  p {
-    margin: 0.5rem 0;
+    cursor: pointer;
   }
 `;
 
@@ -35,20 +86,31 @@ export const SentenceChecker = () => {
     const subscription = checkSentence(currentSentence).subscribe(response => setCheckResponse(response));
     return () => subscription.unsubscribe();
   }, [currentSentence]);
-  return <StyledTextAreaWithButton>
-    <TextareaWithButton
-      textAreaContent={currentSentence}
-      buttonText="CHECK"
-      onButtonClick={(value) => setCurrentSentence(value)}
-    />
-    {checkResponse.state === ResponseState.SUCCESS && checkResponse.result && checkResponse.result.length > 0 && <>
-      <p>Non-English words: {checkResponse.result.join(', ')}</p>
-    </>}
-    {checkResponse.state === ResponseState.SUCCESS && checkResponse.result && checkResponse.result.length === 0 && <>
-      <p>Congrats! This sentence is composed out of valid words only. However, that doesn't mean it is grammatically correct.</p>
-    </>}
-    {checkResponse.state === ResponseState.ERROR && <>
-      <p>Oh snap! An error occurred. Please try again.</p>
-    </>}
-  </StyledTextAreaWithButton>
-}
+  return (
+    <ChatLayout>
+      {checkResponse.state === ResponseState.SUCCESS && checkResponse.result && checkResponse.result.length > 0 && <>
+        <p className="text">Non-English words: {checkResponse.result.join(', ')}</p>
+      </>}
+      {checkResponse.state === ResponseState.SUCCESS && checkResponse.result && checkResponse.result.length === 0 && <>
+        <p className="text">Congrats! This sentence is composed out of valid words only. However, that doesn&apos;t mean it is grammatically correct.</p>
+      </>}
+      {checkResponse.state === ResponseState.ERROR && <>
+        <p className="text">Oh snap! An error occurred. Please try again.</p>
+      </>}
+      {checkResponse.state === ResponseState.PENDING && <>
+        <p className="pending"></p>
+      </>}
+      {checkResponse.state === ResponseState.STALE && <>
+        <div>&nbsp;</div>
+      </>}
+      <StyledTextAreaWithButton>
+        <TextareaWithButton
+          placeholderText="Please type something"
+          textAreaContent={currentSentence}
+          buttonText="CHECK"
+          onButtonClick={(value) => setCurrentSentence(value)}
+        />
+      </StyledTextAreaWithButton>
+    </ChatLayout>
+  );
+};
